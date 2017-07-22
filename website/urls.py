8220d42bbeb35607e3b2e7f175a21c366a64f786
Slyces <simon.lassourreuille@gmail.com>
2017-07-22 19:50:12 +0200

    working on login template

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
+import django.contrib.auth.views as auth_views
 from . import views
 
 urlpatterns = [
-    url(r'^home/?', views.home, name='home'),
+    url(r'^home/?$', views.index, name='home'),
+    url(r'^login/?$', auth_views.login, name='login'),
+    url(r'^logout/?$', auth_views.logout, name='logout'),
     url(r'^currentGame/?', views.currentGame, name='currentGame'),
     url(r'^gameList/?', views.gameList, name='gameList'),
     url(r'^help/?', views.help, name='help'),
