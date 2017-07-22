9aaa082dd274a27f460aace5ced12ab30409301e
Hugo Saint-Vignes <hugo.saint-vignes@student.ecp.fr>
2017-07-22 23:54:59 +0000

    Merge branch 'master' of https://github.com/Slyces/Pleine_Lune
    # Conflicts:
    #	website/models.py

--- website/models.py
+++ website/models.py
@@ -1,30 +1,28 @@
 from django.db import models
 from .views import User
 
-
 # Create your models here.
+
+class Role(models.Model):
+    name = models.TextField(max_length=50, blank=True)
+    description = models.TextField(max_length=500, blank=True)
+    official = models.BooleanField(blank=True)
+    # @TODO profile picture
+
 
 class Player(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     nickname = models.TextField(max_length=50, blank=True)
-    game = models.ManyToManyField(Game)
     role = models.ForeignKey(Role, on_delete=models.CASCADE)
     # @TODO Etat dans le jeu (mort, Cupidon...)
     # @TODO Droits (chans visibles)
     # @TODO profile picture
 
 
-class Role(models.Model):
-    name = models.TextField(max_length=50, blank=True)
-    description = models.TextField(max_length=500, blank=True)
-    officiel = models.BooleanField(blank=True)
-    # @TODO profile picture
-
-
 class Game(models.Model):
     name = models.TextField(max_length=50, blank=True)
     gamemode = models.TextField(max_length=50, blank=True)
-    players = models.ManyToManyField(Player)
+    player = models.ManyToManyField(Player)
     # @TODO Etat du jeu
 
 class Message(models.Model):
