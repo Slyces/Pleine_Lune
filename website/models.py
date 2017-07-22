6415d2a4941e3f93a0d14d78bec9a08be8c0edc4
Hugo Saint-Vignes <hugo.saint-vignes@student.ecp.fr>
2017-07-22 23:40:56 +0000

    Merge branch 'master' of https://github.com/Slyces/Pleine_Lune
    # Conflicts:
    #	website/models.py

--- website/models.py
+++ website/models.py
@@ -5,27 +5,27 @@ from django.db import models
 class Player(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     nickname = models.TextField(max_length=50, blank=True)
-    # profile picture
-    # game
-    # Role
-    # etat
-    # Droits
+    game = models.ManyToManyField(Game)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    # @TODO Etat dans le jeu (mort, Cupidon...)
+    # @TODO Droits (chans visibles)
+    # @TODO profile picture
 
 class Role(models.Model):
     name = models.TextField(max_length=50, blank=True)
     description = models.TextField(max_length=500, blank=True)
     officiel = models.BooleanFieldField(blank=True)
-    # profile_picture
+    # @TODO profile picture
 
 class Game(models.Model):
     name = models.TextField(max_length=50, blank=True)
     gamemode = models.TextField(max_length=50, blank=True)
     players = models.ManyToManyField()
-    # etat
+    # @TODO Etat du jeu
 
 class Message(models.Model):
     content = models.TextField(max_length=500, blank=True)
-    # sender
-    # tag
-    # Date
-    # game
+    sender = models.ForeignKey(Player, on_delete=models.CASCADE)
+    # @TODO tag du message
+    pub_date = models.DateField()
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
