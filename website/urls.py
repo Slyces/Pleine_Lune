4d974afcf97181b3d193dd28fe17fa86fe273caf
DeepBlue\Charly <compte.cobaye@laposte.net>
2017-07-22 19:45:05 +0200

    Création des pages :"currentGame", "gameList" et "help"

--- website/urls.py
+++ website/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^home/?', views.index, name='home'),
+    url(r'^home/?', views.home, name='home'),
+    url(r'^currentGame/?', views.currentGame, name='currentGame'),
+    url(r'^gameList/?', views.gameList, name='gameList'),
+    url(r'^help/?', views.help, name='help'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
+def home(request):
     return render(request, 'website/home.html')
+
+def currentGame(request):
+    return render(request, 'website/currentGame.html')
+
+def gameList(request):
+    return render(request, 'website/gameList.html')
+
+def help(request):
+    return render(request, 'website/help.html')
