e3608b0264328ba7be97c240bad12c868155a498
Slyces <simon.lassourreuille@gmail.com>
2017-07-22 20:24:54 +0200

    urls typo fix

--- website/urls.py
+++ website/urls.py
@@ -3,7 +3,7 @@ import django.contrib.auth.views as auth_views
 from . import views
 
 urlpatterns = [
-    url(r'^home/?$', views.index, name='home'),
+    url(r'^home/?$', views.home, name='home'),
     url(r'^login/?$', auth_views.login, name='login'),
     url(r'^logout/?$', auth_views.logout, name='logout'),
     url(r'^currentGame/?', views.currentGame, name='currentGame'),
