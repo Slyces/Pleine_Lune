fdda3854af96c2c54c3f1e58f8d676c4b8e3f4ef
Hugo Saint-Vignes <hugo.saint-vignes@student.ecp.fr>
2017-07-23 01:18:50 +0000

    Merge branch 'master' of https://github.com/Slyces/Pleine_Lune
    # Conflicts:
    #	website/models.py

--- website/forms.py
+++ website/forms.py
@@ -1,6 +1,15 @@
 from django.contrib.auth.models import User
-from django.contrib.auth import forms
+from django.contrib.auth import forms as auth_forms
 from django import forms
+
 
 class ChampMessage(forms.Form):
     message = forms.CharField(widget=forms.Textarea)
+
+
+class RegisterForm(forms.Form):
+    username = auth_forms.UsernameField(max_length=50)
+    password = forms.CharField(widget=forms.PasswordInput())
+    email = forms.EmailField(max_length=50)
+    first_name = forms.CharField(max_length=50)
+    last_name = forms.CharField(max_length=50)
