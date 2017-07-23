46dddaa8cbd48e7e15334e23c693abbbc9fce9e7
Slyces <simon.lassourreuille@gmail.com>
2017-07-23 20:06:56 +0200

    inclusion des défaults de django.contrib.auth

--- website/urls.py
+++ website/urls.py
@@ -4,11 +4,10 @@ from . import views
 
 urlpatterns = [
     url(r'^home/?$', views.home, name='home'),
-    url(r'^accounts/login/?$', auth_views.login, name='login'),
-    url(r'^accounts/logout/?$', auth_views.logout, name='logout'),
+    # url(r'^accounts/login/?$', auth_views.login, name='login'),
+    # url(r'^accounts/logout/?$', auth_views.logout, name='logout'),
     url(r'^currentGame/(?P<game_id>[0-9]*)/?$', views.current_game, name='currentGame'),
     url(r'^gameList/?', views.game_list, name='gameList'),
     url(r'^help/?', views.help_page, name='help'),
-    url(r'^accounts/registSample Texter', views.register, name='register'),
     url(r'^createGame/', views.create_game, name='createGame'),
 ]
